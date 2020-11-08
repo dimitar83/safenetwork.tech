91a83831aec6b9b26411d6f5a771dfac7c1fe630
dimitar83 <38901442+dimitar83@users.noreply.github.com>
2020-11-08 13:35:46 +0200

    Update content.js

--- src/pages/HowItWorks/content.js
+++ src/pages/HowItWorks/content.js
@@ -33,9 +33,9 @@ export default {
       ],
       aside: {
         position: 1,
-        para: 'Want to try the SAFE Network, and help build the future of the Internet?',
+        para: 'Искате ли да изпробвате Safe мрежата и да помогнете за изграждането на бъдещето на Интернет?',
         CTA: {
-          name: 'Get Involved',
+          name: 'Включете се',
           url: '/get-involved/'
         }
       }
@@ -43,18 +43,18 @@ export default {
       name: 'Съхранение на данни',
       desc: [
         {
-          title: 'Pay once, securely store forever',
+          title: 'Платете веднъж, съхранявайте сигурно завинаги',
           para: [
-            `You're likely to want to store data on the Network. Why? Because in return for a very small one-time Safecoin payment, your data will then be stored forever, encrypted and accessible anywhere in the world and only by you—unless you choose to share it.`
+            `Има вероятност да искате да съхраните информацията си в мрежата. Защо? Защото в замяна на много малко еднократно плащане в Safecoin, вашите данни след това ще се съхраняват завинаги, криптирани и достъпни навсякъде по света и само от вас - освен ако не решите да ги споделите.`
           ],
-          quote: `"Your credentials can't be stolen, because at no point are they ever stored on the Network"`
+          quote: `"Вашите данни за достъп не могат да бъдат откраднати, тъй като в нито един момент те не се съхраняват в мрежата"`
         }, {
-          para: `The Network is permissionless. That means you don't need to apply to a third party to take part—and, crucially, no-one can stop you. This happens  because the SAFE Network has Self-Authentication. It's the only decentralised Network in the world that lets someone securely and anonymously create an account and log in without needing a third party to manage the login process or to store and manage users' credentials. That means your credentials can't be stolen, because at no point are they ever stored on the Network itself.`
+          para: `Свързването с мрежата работи без нужда от разрешение. Това означава, че не е необходимо да искате разрешение от трета страна, за да участвате - и, най-важното, никой не може да ви спре. Постигаме това със Само-Удостоверяване. Safe е единствената децентрализирана мрежа в света, която позволява на някой сам, сигурно и анонимно да създаде акаунт и да влезе, без да се нуждае от трета страна, която да управлява процеса на влизане или да съхранява и управлява идентификационните данни на потребителите. Това означава, че вашите идентификационни данни не могат да бъдат откраднати, защото в нито един момент те не се съхраняват в самата мрежа.`
         }, {
-          title: 'Keeping your data private',
+          title: 'Мрежата пази информацията ви достъпна само за вас',
           para: [
-            `Next, let's talk encryption. Imagine you want to store a photo. That data is protected by a number of layers of encryption. Your photo starts by being broken into pieces which are then encrypted with the other parts of that same file. This 'Self-Encryption' happens before the data ever hits the Network. So, unless you choose to override it, none of your data touches the Network unless it is encrypted. And it’s designed so that you're the only one that ever holds the key.`,
-            `This is a very different approach to blockchain-based storage platforms which often secure just the pointer that shows where the data is stored. With data stored elsewhere, you can prove that data hasn’t been tampered with. But it doesn't protect the data itself. The SAFE Network has a different approach altogether. The data itself is encrypted and stored on the Network. That means no data stores to protect elsewhere.`
+            `Сега нека поговорим за криптирането. Представете си, че искате да качите снимка. Тя е защитена от множество криптиращи слоеве. Качването на снимка Ви започва с разбиване на части, които след това се криптират с другите части на същия файл. Това „Само-Криптиране“ се случва преди данните да се качат в мрежата. Така че, освен ако не решите да го промените, вашите данни никога не докосват мрежата, преди да бъдат криптирани. И тя е проектирана така, че вие да сте единственият, който държи ключа към тях.`,
+            `Това е много различен подход в сравнение с платформите за съхранение на информация базирани на блокчейн, които често защитават само връзката, която показва къде се съхраняват данните. С данните, съхранявани на друго място, можете да докажете, че те не са били подправени. Но не защитавате самите данни. Safe мрежата има различен подход. Самите данни се криптират и съхраняват в мрежата. Това означава, че няма хранилища с данни, които да трябва да се защитават другаде.`
           ]
         }
       ],
@@ -62,21 +62,21 @@ export default {
       name: 'Къде се съхраняват данните?',
       desc: [
         {
-          title: 'Behold, The Serverless Internet',
+          title: 'Вижте, Интернетът без сървъри',
           para: [
-            'The Web is fragile today partly because data is stored in central locations. These become targets for hackers, thieves and data harvesters, or anyone else who wants to snoop around your personal data.',
-            'The SAFE Network doesn’t have these centralised servers. It has no single point which can be attacked, hacked or snooped upon. All uploaded data is broken into chunks, encrypted, dispersed across the Network, and continually moved between computers.',
-            'So how could an attacker search for your photo?  Without reading them, he’d have to know  which encrypted chunks made up any one file; where each piece was stored; control a number of computers that has responsibility for managing each of those different locations on the Network at any one point in time; and then somehow break the encryption (with no password!).',
-            `And that's a simplification. It's actually much harder than that. But that gives you a feel for just how secure data is on the SAFE Network. Once you take on board that it's the Network itself that makes decisions autonomously about locations and who does what, the challenge facing any attacker becomes even clearer.`
+            'Днешният Интернет е уязвим, отчасти защото данните се съхраняват на централизирани места. Те се превръщат в мишени за хакери, крадци и корпорации събиращи лични данни, както и за всеки друг, който иска да се възползва от вашите лични данни.',
+            'Safe мрежата няма централизирани сървъри. Няма нито една точка, която да може да бъде атакувана, хакната или следена. Всички качени данни се разбиват на парчета, криптират се, разпръскват се по мрежата и непрекъснато се местят между компютрите.',
+            'И така, как може нападател да намери ваша снимка? Без да може да разчете отделните криптирани парчета съставящи всеки един файл, той би трябвало да знае точно кои парчета са на снимката; къде се съхранява всяко парче; да контролира няколко компютъра, които са отговорни за управлението на всяко от тези различни места в мрежата във всеки един момент; и след това по някакъв начин да се справи с криптирането (без да знае паролата ви!).',
+            `И това е опростяване. Всъщност е много по-трудно. Но това ви създава усещане за това колко подсигурени са данните в Safe мрежата. Като имате пред очи, и че самата мрежа взема автономни решения относно местоположенията и кой какво да прави, предизвикателството пред всеки нападател става още по-ясно.`
           ]
         }
       ],
       aside: {
         position: 3,
-        title: 'When will it be finished?',
-        para: 'We’re at the dawn of a new Web, and the final pieces of the SAFE Network are being put in place',
+        title: 'Кога ще бъде завършена?',
+        para: 'Ние сме в зората на нов Интернет и изграждаме финалните части на SAFE мрежата.',
         CTA: {
-          name: 'See the Roadmap',
+          name: 'Вижте плана на проекта',
           url: '/roadmap/'
         }
       }
@@ -84,11 +84,11 @@ export default {
       name: 'Кой съхранява вашите данни?',
       desc: [
         {
-          title: 'The Web hosted by everyone, controlled by no-one',
+          title: 'Интернет собственост на всички, контролиран от никой',
           para: [
-            'With no centralised storage, data is instead stored in Vaults. People download free Vault software to their home computers which tells the Network that they’re happy to take part. By providing storage and bandwidth to support the Network, you can get rewarded in Safecoin, the cryptocurrency that powers the Network.',
-            'The Network just makes use of the space you’ve offered and asks you to manage whichever chunks of encrypted data it needs to ensure the system is safe and resilient. The key point here is that no-one (either computers or Network) can read this data. Each chunk of encrypted data is managed by its own Group. The Network decides which computers should be members of which Groups and this changes frequently. And at no time can a person who runs a Vault read any of the data that his computer is storing.',
-            'By running a Vault, your computer races to return any data that is requested by its owner. The rewards paid out by the system will be varied according the Network which varies the price of Safecoin in order to balance the Network in such a way that it consistently maintains around 30% of Vaults unused.'
+            'В мрежата няма централизирано хранилище, данните се съхраняват в Трезори. Хората изтеглят безплатен софтуер наречен Трезор на домашните си компютри, което казва на мрежата, че желаят да участват. Осигурявайки място за съхранение и интернет свързаност в подкрепа на мрежата, можете да получите възнаграждение в Safecoin - криптовалутата, която захранва мрежата.',
+            'Мрежата просто използва пространството, което сте предложили и управлява парчета криптирани данни, от които се нуждаете, за да гарантира, че системата е безопасна и устойчива. Ключовият момент тук е, че никой (нито компютрите, нито мрежата) не може да чете тези данни. Всяка част от криптираните данни се управлява от отделна Секция от Трезори. Мрежата решава кои компютри да бъдат членове на кои Секции и това често се променя. Така в нито един момент човек, който е стартирал Трезор на компютъра си, не може да прочете някои от данните, които неговият компютър съхранява.',
+            'Чрез пускане на Трезор, вашият компютър се надпреварва в доставката на всякакви данни, поискани от собственика им. Възнагражденията, изплащани от системата, ще се променят от мрежата, която променя цената на Safecoin, за да балансира себе си по такъв начин, че да поддържа постоянно около 30% от Трезорите свободни.'
           ]
         }
       ]
@@ -96,15 +96,15 @@ export default {
       name: 'Мрежова архитектура',
       desc: [
         {
-          title: 'Anatomy of an Autonomous Network',
+          title: 'Анатомията на една автономна мрежа',
           para: [
-            'The SAFE Network is self-managing. It manages how computers can act on the Network in real-time, changing roles and responsibilities dynamically in response to real-time changes without any input from humans or businesses.',
-            'The Network is built on a foundation of open source software that allows computers to connect directly to each other, anywhere around the world. The Routing layer sits above this and sets the rules of the Network. This means that computers will act in a certain way that guarantees the Network will function efficiently without a centralised administrator.',
-            'These rules ensure that computers reach agreement on what is and isn’t allowed, and the order in which they happen. Decisions are made by computers in standalone Sections that have a history of good behaviour and thanks to a cutting-edge DAG-based consensus algorithm called PARSEC, the Network is guaranteed to reach agreement despite vast numbers of events taking place simultaneously every second around the world.'
+            'Safe мрежата се самоуправлява. Тя решава в реално време как компютрите могат да действат в мрежата, променяйки динамично ролите и отговорностите на отделните участници в отговор на промените в реално време, без никакво влияние от хора или фирми.',
+            'Мрежата е изградена на основата на софтуер с отворен код, който позволява на компютрите да се свързват директно помежду си, навсякъде по света. Маршрутизиращият слой седи над това и задава правилата на мрежата. Това означава, че компютрите ще действат по определен начин, което гарантира, че мрежата ще функционира ефективно без централизиран администратор.',
+            'Тези правила гарантират, че компютрите постигат съгласие относно това какво е позволено и какво не, както и върху реда, в който се случва. Решенията се вземат от компютри в самостоятелни секции, които имат история на добро поведение и благодарение на иновативен алгоритъм за консенсус, базиран на DAG, наречен PARSEC, мрежата е гарантирано да постигне съгласуваност, въпреки огромния брой събития, които се провеждат едновременно всяка секунда по света.'
           ]
         }, {
-          title: 'Global Communication, Global Scale',
-          para: 'Whilst Sections stand alone, the Network is designed to enable trusted communications. It does this by ensuring that any contact between unknown computers is accompanied by cryptographic proof that every computer involved is a valid part of the Network. With Secure Message Relay comes Data Chains, a novel way which ensures that vast quantities of data can be passed reliably and securely between different nodes that have never known each other previously. All of this is made possible by the key design decision that we started with: all data is stored on the Network. Encrypted and ready to scale the globe from Day One.'
+          title: 'Глобална комуникация, глобален размер',
+          para: 'Докато Секциите са самостоятелни, мрежата е проектирана да позволява надеждни комуникации. Това се постига, като се гарантира, че всеки контакт между непознати компютри е придружен от криптографско доказателство, че всеки свързан компютър е валидна част от мрежата. Със Сигурно Предаване на Съобщенията идват и Веригите от Данни, нова технология, която гарантира, че огромни количества данни могат да бъдат предавани надеждно и сигурно между различни Трезори, които никога не са се свързвали преди. Всичко това става възможно от ключовото решение в дизайна, с което започнахме: всички данни се съхраняват в мрежата. Криптирана и готово за мащабно разрастване по земното кълбо още от първия ден.'
         }
       ]
     }
